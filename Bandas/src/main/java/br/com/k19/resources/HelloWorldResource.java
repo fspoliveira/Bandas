12245d1caaf6debf3cdcf39e578f0341b8f78ffa
fspoliveira <fspoliveira@bitwaysystem.com.br>
2012-09-27 00:32:58 -0300

    1.0

---
+++ Bandas/src/main/java/br/com/k19/resources/HelloWorldResource.java
@@ -0,0 +1,14 @@
+package br.com.k19.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/helloworld")
+public class HelloWorldResource {
+	@GET
+	@Produces("text/plain")
+	public String showHelloWorld() {
+		return "Olá mundo!";
+	}
+}
